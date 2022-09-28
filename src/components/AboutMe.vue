21f54a2dd7385e407818b4a66a64f0f4fa5a870e
Finira <finiraf@gmail.com>
2022-09-28 19:02:14 +0300

    description

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -13,7 +13,8 @@
       </div>
 
       <div class="about-me__description">
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        Passionate Frontend developer with 2.5+ years of experience in creating web applications. I have the following qualities: communication skills, attentiveness, responsibility, diligence, excellent problem solving and task completion.
+        <br> Great desire to learn new things and develop.
       </div>
     </div>
   </div>
